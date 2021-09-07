94a3d8987113f7e9a1441611125a4b91654af366
Francisco Valencia Barchin <pakit@MBPdeFrancisco2.Valtar2>
2021-09-07 22:36:10 +0200

    Arkanoid jugable a falta de puntuación y records

--- arkanoid/entidades.py
+++ arkanoid/entidades.py
@@ -1,25 +1,46 @@
-from arkanoid import ALTO, ANCHO
+from arkanoid import ALTO, ANCHO, FPS
 import pygame as pg
 from pygame.sprite import Sprite
 
 class Raqueta(Sprite):
-    skin = "electric00.png"
+    skin = ["electric00.png","electric01.png","electric02.png"]
     def __init__(self, **kwargs):
-        self.image = pg.image.load(f"resources/images/{self.skin}")
+        super().__init__() #necesario introducir el init de sprite para poder generar group
+        self.imagenes = []
+        for archivo in self.skin:
+            self.imagenes.append(pg.image.load(f"resources/images/{archivo}"))
+        self.imagen_activa = 0
+
+        self.tiempo_transcurrido = 0
+        self.tiempo_cambio = 1000 // FPS * 5
+
+        self.image = self.imagenes[self.imagen_activa]
+
         self.rect = self.image.get_rect(**kwargs)
 
-    def update(self):
+    def update(self, dt):
         if pg.key.get_pressed()[pg.K_LEFT] and self.rect.left > 0:
             self.rect.x -= 5
 
         if pg.key.get_pressed()[pg.K_RIGHT] and self.rect.right < ANCHO:
             self.rect.x +=5
 
+        self.tiempo_transcurrido += dt
+        if self.tiempo_transcurrido >= self.tiempo_cambio:
+            self.imagen_activa += 1
+            if self.imagen_activa >= len(self.imagenes):
+                self.imagen_activa = 0
+            
+            self.tiempo_transcurrido = 0
+        
+        self.image = self.imagenes[self.imagen_activa]
+
 
 class Bola(Sprite):
     skin = "ball1.png"
     
     def __init__(self, **kwargs):
+        super().__init__()
         self.image = pg.image.load(f"resources/images/{self.skin}")
         self.rect = self.image.get_rect(**kwargs)
         self.velocidad_x = 5
@@ -27,7 +48,7 @@ class Bola(Sprite):
         self.viva = True
         self.posicion_inicial = kwargs
 
-    def update(self):
+    def update(self, dt):
 
         self.rect.x += self.velocidad_x
         self.rect.y += self.velocidad_y
@@ -40,13 +61,27 @@ class Bola(Sprite):
 
         if self.rect.bottom >= ALTO:
             self.viva = False
-            self.rect = self.image.get_rect(**self.posicion_inicial)
-        
-            
+            self.reset()
+
+    def reset(self):
+        self.rect = self.image.get_rect(**self.posicion_inicial)
+        self.velocidad_x = 5
+        self.velocidad_y = 5
+       
 
     def choque(self,objeto):
-        if self.rect.right >= objeto.rect.left and self.rect.left <= objeto.rect.right and self.rect.bottom >= objeto.rect.top and self.rect.top<= objeto.rect.bottom:
+        if self.rect.right >= objeto.rect.left and self.rect.left <= objeto.rect.right and self.rect.bottom >= objeto.rect.top and self.rect.top <= objeto.rect.bottom:
             self.velocidad_y *= -1
+
+class Ladrillo(Sprite):
+    skin = "greenTile.png"
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pg.image.load(f"resources/images/{self.skin}")
+        self.rect = self.image.get_rect(x=x, y=y)
+
+
+    
 
 
 
--- arkanoid/escenas.py
+++ arkanoid/escenas.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from . import FPS, ANCHO, ALTO
-from .entidades import Raqueta, Bola
+from .entidades import Raqueta, Bola, Ladrillo
 
 class Escena():
     def __init__(self,pantalla):
@@ -43,22 +43,50 @@ class Partida(Escena):
         super().__init__(pantalla)
         #self.vidas = 3
         self.fondo = pg.image.load("resources/images/background.jpg")
+        self.entidades = pg.sprite.Group()
         self.player = Raqueta(midbottom=(ANCHO // 2, ALTO - 15) )
         self.bola = Bola(center=(ANCHO // 2, ALTO // 2) )
+
+        self.puntos = 0
+
+        #self.l1 = Ladrillo(30,10)
+        #self.l2 = Ladrillo(120,10)
+        #self.l3 = Ladrillo(210,10)
+        #self.l4 = Ladrillo(300,10)
+        #self.l5 = Ladrillo(390,10)
+        #self.l5 = Ladrillo(480,10)
+
+        self.ladrillos = pg.sprite.Group()
+        for f in range(3):
+            
+            for c in range(6):
+                ladrillo = Ladrillo(c*90+30, f * 30 + 10)
+                self.ladrillos.add(ladrillo)
+
+        self.entidades.add(self.ladrillos,self.player,self.bola)
+        
 
     def bucle_principal(self):
         vidas = 3
         while vidas > 0:
-            self.reloj.tick(FPS)
+            dt = self.reloj.tick(FPS)
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     #game_over = True 
                     exit()       
 
-            self.player.update() 
-            self.bola.update()
+            self.entidades.update(dt) 
+            #self.bola.update(dt)
 
-            self.bola.choque(self.player)  
+            self.bola.choque(self.player)
+
+            tocados = pg.sprite.spritecollide(self.bola, self.ladrillos, True)
+            if len(tocados) > 0:
+                self.bola.velocidad_y *= -1
+                self.puntos += len(tocados)*5
+            '''
+            for ladrillo in self.ladrillos:
+                self.bola.choque(ladrillo)'''
 
             if not self.bola.viva:
                 vidas -= 1
@@ -67,8 +95,12 @@ class Partida(Escena):
              
 
             self.pantalla.blit(self.fondo,(0,0))
-            self.pantalla.blit(self.player.image, self.player.rect)
+            self.entidades.draw(self.pantalla)
+            '''self.pantalla.blit(self.player.image, self.player.rect)
             self.pantalla.blit(self.bola.image, self.bola.rect)
+
+            for ladrillo in self.ladrillos:
+                self.pantalla.blit(ladrillo.image, ladrillo.rect)'''
 
             pg.display.flip()
 
